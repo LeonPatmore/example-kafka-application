bac549f4f70cdb3db3a0ccd42d05e861343c8558
Leon Patmore <leon.patmore@vonage.com>
2020-04-01 11:28:42 +0100

    add readme

---
+++ scripts/config.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+export KAFKA_BIN_DIR="/Users/lpatmore/Documents/kafka/bin"
--- scripts/send-request.sh
+++ scripts/send-request.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-KAFKA_BIN_DIR="/Users/lpatmore/Documents/kafka/bin"
+source config.sh
 
 cd ${KAFKA_BIN_DIR} && ./kafka-console-producer.sh --broker-list localhost:9092 --topic test-topic
